f6ca77d18262a4e15efbeb39a2c1f8deeada0231
Akong <paramonych@gmail.com>
2023-01-01 21:23:29 +0700

    Added tags proper coloring

--- src/scripts/Tags.svelte
+++ src/scripts/Tags.svelte
@@ -1,24 +1,25 @@
 <script lang="ts">
   import { createEventDispatcher } from "svelte"
   import { BIO } from "./values"
-  
+  import { isTooDark } from "./utils"
   
   export let options = []
   export let selectedTag
   export let colors
-  
+
+  let selectedColor = colors[options.indexOf(selectedTag)]
   let dispatch = createEventDispatcher()
   
   $: {
-
+    selectedColor = colors[options.indexOf(selectedTag)]
     dispatch('tagChanged', selectedTag)
   }
 </script>
 
 <div class="tags-manager">
-  <select bind:value={selectedTag} style='background-color: {colors[options.indexOf(selectedTag)]};'>
+  <select bind:value={selectedTag} style='background-color: {selectedColor};' class:too-dark={isTooDark(selectedColor)}>
     {#each options as option, index}
-      <option value={option} disabled={option == BIO.DEFAULT} selected={option == BIO.DEFAULT} style='background-color: {colors[index]};'>
+      <option value={option} disabled={option == BIO.DEFAULT} selected={option == BIO.DEFAULT} style='background-color: {colors[index]};' class:too-dark={isTooDark(colors[index])}>
         {option}
       </option>
     {/each}
--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -15,3 +15,12 @@ export function getSelection() {
 export function randomColor() {
     return `#${Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase()}`
 }
+
+export function isTooDark(hexcolor) {
+    let r = parseInt(hexcolor.substr(1,2),16);
+    let g = parseInt(hexcolor.substr(3,2),16);
+    let b = parseInt(hexcolor.substr(5,2),16);
+    let yiq = ((r*299)+(g*587)+(b*114))/1000;
+    
+    return (yiq < 70);
+}
